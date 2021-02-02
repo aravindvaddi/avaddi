4b61ceff6fa81de7e1cc3d86312fad246732b71b
Aravind Vaddi <aravindvaddi.github@gmail.com>
2021-02-02 18:22:43 -0500

    Update icons

--- components/icons.js
+++ components/icons.js
@@ -7,9 +7,11 @@ export default function Icons(props) {
 			<svg xmlns='http://www.w3.org/2000/svg' className={styles.navbarIcon} width='24' height='24' viewBox='0 0 24 24'>
 				{
 					props.type === 'resume' ? (
-						<path fill= '#d4cec2'  d='M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z'/>
+						<path fill= '#d4cec2' d='M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z'/>
 					) : props.type === 'contact' ? (
 						<path fill= '#d4cec2' d='M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z'/>
+					) : props.type === 'projects' ? (
+						<path fill= '#d4cec2' d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"/>
 					) : (
 						<path fill= '#d4cec2' d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z'/>
 					)
